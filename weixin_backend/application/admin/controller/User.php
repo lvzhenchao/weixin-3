ffc00d4f83172d8e7dcc112b8089fd361ab6db27
锐源 <1370164516@qq.com>
2023-10-31 00:46:48 +0800

    用户分页

--- weixin_backend/application/admin/controller/User.php
+++ weixin_backend/application/admin/controller/User.php
@@ -3,10 +3,13 @@
 namespace app\admin\controller;
 
 use think\Controller;
+use think\Db;
 
 class User extends Base{
     //
 	public function index(){
+        $users = Db::name('user')->paginate(10);
+        $this->assign('users', $users);
 		return $this->fetch();
 	}
 }
